5b2422fe09186413f5275eee165ec683c73e6097
Shane Bair <Shane_Bair@cable.comcast.com>
2016-12-07 07:43:02 -0500

    Highscore api functionality v1

---
+++ AlexRuns.Web/Controllers/Api/HighScoresController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AlexRuns.Web.Controllers.Api
+{
+    using Models;
+    using Services;
+
+    public class HighScoresController : ApiController
+    {
+        // GET: api/HighScores
+        [HttpGet]
+        public IEnumerable<HighScore> Get()
+        {
+            var highscoreService = new HighScoreService();
+
+            return highscoreService.GetHighScores();
+        }
+
+        // POST: api/HighScores
+        [HttpPost]
+        public void Post([FromBody]HighScore score)
+        {
+            var highscoreService = new HighScoreService();
+
+            highscoreService.AddHighScore(score);
+        }
+
+        // PUT: api/HighScores/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/HighScores/5
+        public void Delete(int id)
+        {
+            var highscoreService = new HighScoreService();
+
+            highscoreService.DeleteHighScore(id);
+        }
+    }
+}
---
+++ AlexRuns.Web/Services/HighScoreService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlexRuns.Web.Services
+{
+    using LiteDB;
+    using Models;
+
+    public class HighScoreService
+    {
+        public IEnumerable<HighScore> GetHighScores(int count = 10)
+        {
+            var scores = new List<HighScore>();
+
+            try
+            {
+                using (var db = new LiteDatabase("C:/DevPersonal/Games/craftyjs/AlexRunsForever/AlexRuns.Web/App_Data/scores.db"))
+                {
+                    var scoresCollection = db.GetCollection<HighScore>("HighScore");
+                    scores = scoresCollection.FindAll().OrderBy(hs => hs.Score).ToList();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return scores;
+        }
+
+        public void AddHighScore(HighScore score)
+        {
+            try
+            {
+                if (score != null)
+                {
+                    using (var db = new LiteDatabase("C:/DevPersonal/Games/craftyjs/AlexRunsForever/AlexRuns.Web/App_Data/scores.db"))
+                    {
+                        var scoresCollection = db.GetCollection<HighScore>("HighScore");
+
+                        scoresCollection.Insert(score);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        public void DeleteHighScore(int id)
+        {
+            try
+            {
+                using (var db = new LiteDatabase("C:/DevPersonal/Games/craftyjs/AlexRunsForever/AlexRuns.Web/App_Data/scores.db"))
+                {
+                    var scoresCollection = db.GetCollection<HighScore>("HighScore");
+
+                    scoresCollection.Delete(id);
+                }
+            }
+            catch(Exception ex) { }
+        }
+    }
+}
